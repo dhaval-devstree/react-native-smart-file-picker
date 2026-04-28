3f5d6e5a5ab3ac47583f67608d9e2df0a824a1d0
Dhaval <dhaval.b@devstree.in>
2026-04-28 11:55:39 +0530

    Added cache management methods to clear and retrieve file picker cache path

--- ExampleApp/App.js
+++ ExampleApp/App.js
@@ -1,10 +1,16 @@
 // Global Imports
+import { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { clearSmartFilePickerCache } from 'react-native-smart-file-picker';
 
 // Local Imports
 import AppNavigator from './AppNavigator';
 
 const App = (props) => {
+
+  useEffect(() => {
+    clearSmartFilePickerCache().catch(() => { });
+  }, []);
 
   return (
     <SafeAreaProvider style={{ flex: 1 }}>
